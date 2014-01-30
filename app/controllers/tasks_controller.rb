75da5582be8100a934e7eaf0b393cce18de8deab
Akshay Khole <akshay@idyllic-software.com>
2014-01-30 11:26:16 +0530

    scaffolding task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,73 +1,63 @@
 class TasksController < ApplicationController
-  before_action :set_task, :except => [:create_task, :index, :delete_task]
-  skip_before_filter :verify_authenticity_token
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
+  # GET /tasks
+  # GET /tasks.json
   def index
-    respond_to do |format|
-      format.json {
-        render json: Task.all
-      }
-      format.html {
-        render html: Task.all
-      }
-    end
+    @tasks = Task.all
   end
 
+  # GET /tasks/1
+  # GET /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
   def new
+    @task = Task.new
   end
 
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks
+  # POST /tasks.json
   def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @task }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
   def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
   def destroy
-  end
-
-  def fetch_task
+    @task.destroy
     respond_to do |format|
-      format.json {
-        render json: @task
-      }
-      format.html{
-        render :fetch_task
-      }
-    end
-  end
-
-  def create_task
-    title = task_params[:title]
-    description = task_params[:description]
-    task = Task.create!(:title => title, :description => description)
-    respond_to do |format|
-      format.json {
-        render json: task
-      }
-    end
-  end
-
-  def update_task
-    task = Task.find(params[:id])
-    task.title = task_params[:title]
-    task.description = task_params[:description]
-    if task.save
-      respond_to do |format|
-        format.json {
-          render json: task
-        }
-      end
-    end
-  end
-
-  def delete_task
-    task = Task.find(params[:id])
-    if task.destroy
-      respond_to do |format|
-        format.json {
-          render json: {'status' => 'deleted'}
-        }
-      end
+      format.html { redirect_to tasks_url }
+      format.json { head :no_content }
     end
   end
 
@@ -79,8 +69,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params[:task].permit(:title, :description)
-    rescue
-      params.permit(:title, :description)
+      params[:task].permit(:title, :descr)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 TestApp::Application.routes.draw do
   resources :tasks
-  get '/tasks' => 'tasks#index'
-  get '/fetch_task' => "tasks#fetch_task"
-  put '/update_task' => "tasks#update_task"
-  delete '/delete_task' => "tasks#delete_task"
-  post '/create_task' => "tasks#create_task"
   root to: "tasks#index"
 end
